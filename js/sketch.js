a71994b4b298425336ca5a4f28c9d86ea955e06d
Carleydmar <93782069+Carleydmar@users.noreply.github.com>
2022-01-13 21:03:47 +0100

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,52 @@
+var brushSize, brushShape, paintColour;
+var coloursArray = ['red', 'green', 'blue'];
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(800, 450);
     canvas.parent("p5container");
+
+    brushSize = 10;
+
+    background(220);
+
+    paintColour = coloursArray[2];
+
 }
 
 function draw() {
-    background(220);
+    
+    if(paintColour === 'red'){
+        fill('rgb(255, 0, 0)');
+    } else if(paintColour === 'green'){
+        fill('rgb(0, 255, 0)');
+        } else if(paintColour === 'blue'){
+            fill('rgb(0, 0, 255)');
+         }
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    noStroke();
+    
+
+    if(mouseIsPressed == true){
+        ellipse(mouseX, mouseY, brushSize);
+    }
+  
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW && brushSize <= 50){
+        brushSize = brushSize + 5;
+    } else {
+        if(keyCode === DOWN_ARROW && brushSize >= 10){
+         brushSize = brushSize - 5;   
+        }
+    }
+
+    if(keyCode === 81){
+        paintColour = paintArray[0];
+    }
+
+    if(keyCode === 87){
+        paintColour = colourArray[1];
+    }
+
 }
